a1ba492ff57145377370c8eb11ea177456749b29
Dakshina Scott <dakshina.scott@gmail.com>
2017-08-19 08:56:07 +0100

    Serves /register page relates #2

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const login = require('./login');
+const register = require('./register');
 
 router.get('/login', login.get);
+router.get('/register', register.get);
 
 module.exports = router;
--- tests/backend.test.js
+++ tests/backend.test.js
@@ -14,7 +14,18 @@ test('/login get', (t) => {
     .expect(200)
     .end((err, res) => {
       t.equals(res.status, 200, 'Responds with 200 status');
-      t.ok(res.text.includes('login'), 'Page contains string \'login\'');
+      t.ok(res.text.includes('login'), 'Page contains string \'<h2>Login</h2>\'');
+      t.end();
+    });
+});
+
+test('/register get', (t) => {
+  supertest(app)
+    .get('/register')
+    .expect(200)
+    .end((err, res) => {
+      t.equals(res.status, 200, 'Responds with 200 status');
+      t.ok(res.text.includes('<h2>Register</h2>'), 'Page contains string \'<h2>Register</h2>\'');
       t.end();
     });
 });
