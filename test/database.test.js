26c618b45ea30d8674bbfb52dab582b2a1a6b38b
Dakshina Scott <dakshina.scott@gmail.com>
2017-08-21 10:38:38 +0100

    Adds missing brackets

--- test/database.test.js
+++ test/database.test.js
@@ -8,17 +8,16 @@ const insertUser = require('../queries/insertUser');
 const getUser = require('../queries/getUser');
 
 test('Insert user into database', (t) => {
-  dbReset(
+  dbReset(() => {
     insertUser('james', 'james@gmail.com', 'jammy', (err, result) => {
-      console.log(result);
       t.equal(typeof result[0].id, 'number', 'Returns an array containing the user\'s id');
       t.end();
-    });
-  );
+    })
+  });
 });
 
 test('Get user from database based on email', (t) => {
-  dbReset(
+  dbReset(() => {
     const expected = {
       name: 'james',
       email: 'james@gmail.com',
@@ -30,7 +29,7 @@ test('Get user from database based on email', (t) => {
           t.equal(result[0].key, expected.key, `Returns object with same ${key}`);
         });
         t.end();
-      });
+      })
     });
-  );
+  });
 });
