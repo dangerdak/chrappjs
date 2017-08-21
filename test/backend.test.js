c5cfe599f3ff22789515a1d5ddaf74c5c025d5ff
Dakshina Scott <dakshina.scott@gmail.com>
2017-08-21 20:08:16 +0100

    Removes register/post test relates #18

--- test/backend.test.js
+++ test/backend.test.js
@@ -1,6 +1,7 @@
 const test = require('tape');
 const supertest = require('supertest');
 
+const dbReset = require('../database/db_build');
 const app = require('./../src/app');
 
 test('/login get', (t) => {
@@ -34,18 +35,6 @@ test('/groups get', (t) => {
     .end((err, res) => {
       const title = '<h2>Your Groups</h2>';
       t.equals(res.status, 200, 'Responds with 200 status');
-      t.ok(res.text.includes(title), `Page contains string ${title}`);
-      t.end();
-    });
-});
-
-test('/register post', (t) => {
-  supertest(app)
-    .get('/register')
-    .expect(200)
-    .end((err, res) => {
-      const title = '<h2>Your Groups</h2>';
-      t.equals(res.status, 302, 'Responds with 302 status');
       t.ok(res.text.includes(title), `Page contains string ${title}`);
       t.end();
     });
