ae017b2c0798e37975f4f2c324bcb8c9ec17dde3
Dakshina Scott <dakshina.scott@gmail.com>
2017-08-21 08:22:11 +0100

    Tests must happen after db reset finished

--- database/db_build.js
+++ database/db_build.js
@@ -4,9 +4,12 @@ const dbConnection = require('./db_connection');
 
 const sqlBuild = fs.readFileSync(path.join(__dirname, 'db_build.sql'), 'utf8');
 
-const build = () => {
+const build = (cb) => {
   dbConnection.query(sqlBuild, (err, res) => {
     if (err) throw err;
+    if (cb) {
+      cb();
+    }
     console.log('Database build successful.');
   });
 }
