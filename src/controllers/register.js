142b122e13f202fccafc890f2e85167640fe4285
Dakshina Scott <dakshina.scott@gmail.com>
2017-08-22 07:32:55 +0100

    If/else is more clear

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -6,16 +6,20 @@ exports.get = (req, res) => {
 }
 exports.post = (req, res) => {
   const formData = req.body;
+  // TODO validate form data, check if email already exists
+  // TODO try/catch for use of sign function?
   const hashedPassword = sign(formData.password);
   insertUser(formData.name, formData.email, hashedPassword, (err, result) => {
     if (err) {
+      console.log(err);
       res.status(500).render('error', {
         layout: 'error',
         statusCode: 500,
         errorMessage: 'Internal server error',
       });
-      return console.log(err);
     }
-    res.redirect('groups');
+    else {
+      res.redirect('groups');
+    }
   })
 };
