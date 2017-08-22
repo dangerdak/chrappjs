4a82dab63c756641e932c3ff0112f33cb884d9b2
Dakshina Scott <dakshina.scott@gmail.com>
2017-08-22 13:54:23 +0100

    Adds favicon

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
 
 // import controllers/routes
 const controllers = require('./controllers/index');
@@ -21,6 +22,8 @@ app.engine(
 );
 
 app.set('port', process.env.PORT || 3000);
+app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.png')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(controllers);
 
