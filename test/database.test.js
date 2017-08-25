21ed854d2061e057c04c15ff7eaaf87fbc28bfd1
Dakshina Scott <dakshina.scott@gmail.com>
2017-08-25 11:27:15 +0100

    Test for insert group query relates #28

--- test/database.test.js
+++ test/database.test.js
@@ -6,14 +6,15 @@ const dbReset = require('../database/db_build');
 const dbConnection = require('../database/db_connection');
 const insertUser = require('../queries/insertUser');
 const getUser = require('../queries/getUser');
+const insertGroup = require('../queries/insertGroup');
 
 test('Insert user into database', (t) => {
   dbReset()
     .then(() => {
       return insertUser('james', 'james@gmail.com', 'jammy')
     })
-    .then(id => {
-      t.equal(typeof id, 'number', 'Returns the user\'s id');
+    .then(userId => {
+      t.equal(typeof userId, 'number', 'Returns the user\'s id');
       t.end();
     })
     .catch(err => {
@@ -40,6 +41,34 @@ test('Get user from database based on email', (t) => {
       });
     })
     .then(() => {
+      t.end();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+});
+
+test('Insert group into database', (t) => {
+  dbReset()
+    .then(() => {
+      return insertUser('james', 'james@gmail.com', 'jammy')
+    })
+    .then(userId => {
+      const today = new Date();
+      const futureDate = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()+1}`;
+
+      const groupInfo = {
+        name: 'Crabbies',
+        description: 'Xmas pressies for all',
+        budget: 80,
+        is_assigned: false,
+        deadline: futureDate,
+      };
+      return insertGroup(userId, groupInfo)
+    })
+    .then(idObj => {
+      t.equal(typeof idObj.group_id, 'number', 'Returns an object containing the group\'s id');
+      t.equal(typeof idObj.user_id, 'number', 'Returns an object containing the user\'s id');
       t.end();
     })
     .catch(err => {
