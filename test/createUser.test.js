62b0c66b262670628558a30d900fb92f142ca29d
Dakshina Scott <dakshina.scott@gmail.com>
2018-01-30 11:58:30 +0000

    #40 reset db before all tests which touch db

--- test/createUser.test.js
+++ test/createUser.test.js
@@ -3,19 +3,26 @@ const test = require('tape');
 const createUser = require('../src/lib/createUser');
 const getUser = require('../queries/getUser');
 
-test('createUser', (t) => {
-  t.plan(2);
-  const input = {
-    name: 'bill',
-    email: 'bill@gmail.com',
-    password: 'password',
-  };
-  createUser(input.name, input.email, input.password)
-    .then((id) => {
-      t.equals(typeof id, 'number', 'Returns id of created user');
-      return getUser(input.email);
-    }).then((userData) => {
-      t.notEqual(userData.password, input.password, 'Doesnt store plain text password');
-    })
-    .catch(console.log);
+const { QueryFile } = require('pg-promise');
+
+const seedFile = new QueryFile(path.join(__dirname, '..', 'database', 'db_seed.sql'), { minify: true });
+const dbReset = require('../database/db_build').bind(null, seedFile);
+
+dbReset().then(() => {
+  test('createUser', (t) => {
+    t.plan(2);
+    const input = {
+      name: 'bill',
+      email: 'bill@gmail.com',
+      password: 'password',
+    };
+    createUser(input.name, input.email, input.password)
+      .then((id) => {
+        t.equals(typeof id, 'number', 'Returns id of created user');
+        return getUser(input.email);
+      }).then((userData) => {
+        t.notEqual(userData.password, input.password, 'Doesnt store plain text password');
+      })
+      .catch(console.log);
+  });
 });
