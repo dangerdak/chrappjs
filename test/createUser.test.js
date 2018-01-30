a90c058afe7c1a470d8a3cebb4e4027252ac1478
Dakshina Scott <dakshina.scott@gmail.com>
2018-01-30 12:14:41 +0000

    #40 #43 fixes createUser test

--- test/createUser.test.js
+++ test/createUser.test.js
@@ -4,21 +4,19 @@ const dbReset = require('../database/db_build').seed;
 const createUser = require('../src/lib/createUser');
 const getUser = require('../queries/getUser');
 
-dbReset().then(() => {
-  test('createUser', (t) => {
-    const input = {
-      name: 'bill',
-      email: 'bill@gmail.com',
-      password: 'password',
-    };
-    createUser(input.name, input.email, input.password)
-      .then((id) => {
-        t.equals(typeof id, 'number', 'Returns id of created user');
-        return getUser(input.email);
-      }).then((userData) => {
-        t.notEqual(userData.password, input.password, 'Doesnt store plain text password');
-        t.end();
-      })
-      .catch(console.log);
-  });
+test('createUser', (t) => {
+  const input = {
+    name: 'bill',
+    email: 'bill@gmail.com',
+    password: 'password',
+  };
+  dbReset().then(() => createUser(input.name, input.email, input.password))
+    .then((id) => {
+      t.equals(typeof id, 'number', 'Returns id of created user');
+      return getUser(input.email);
+    }).then((userData) => {
+      t.notEqual(userData.password, input.password, 'Doesnt store plain text password');
+      t.end();
+    })
+    .catch(console.log);
 });
