bbeb08f632463f410c8fb54aeff24073c86fcd8f
Dakshina Scott <dakshina.scott@gmail.com>
2018-01-30 11:54:45 +0000

    #40 reset db before testing checkLogin.js

--- test/checkLogin.test.js
+++ test/checkLogin.test.js
@@ -1,29 +1,36 @@
 const test = require('tape');
+const path = require('path');
+const { QueryFile } = require('pg-promise');
+
+const seedFile = new QueryFile(path.join(__dirname, '..', 'database', 'db_seed.sql'), { minify: true });
+const dbReset = require('../database/db_build').bind(null, seedFile);
 
 const checkLogin = require('../src/lib/checkLogin');
 
-test('checkLogin', (t) => {
-  t.plan(4);
-  checkLogin('sam@gmail.com', 'password')
-    .then((actual) => {
-      const expected = {
-        id: 1,
-        name: 'sam',
-        email: 'sam@gmail.com',
-        password: '$2a$10$CEicRuoB3hvCnlDx9Of/deXIiRInjoRhYuC9VKdox7n0zVXMbzJb2',
-      };
-      t.ok(actual, 'Returns truthy value if login successful');
-      t.deepEqual(actual, expected, 'Returns correct user data if login successful');
-    })
-    .then(() =>
-      checkLogin('samm@gmail.com', 'password'))
-    .then((actual) => {
-      t.equal(actual, undefined, 'Returns undefined if email incorrect');
-    })
-    .then(() =>
-      checkLogin('sam@gmail.com', 'password1'))
-    .then((actual) => {
-      t.equal(actual, undefined, 'Returns undefined if password incorrect');
-    })
-    .catch(console.log);
+dbReset.then(()=> {
+  test('checkLogin', (t) => {
+    t.plan(4);
+    checkLogin('sam@gmail.com', 'password')
+      .then((actual) => {
+        const expected = {
+          id: 1,
+          name: 'sam',
+          email: 'sam@gmail.com',
+          password: '$2a$10$CEicRuoB3hvCnlDx9Of/deXIiRInjoRhYuC9VKdox7n0zVXMbzJb2',
+        };
+        t.ok(actual, 'Returns truthy value if login successful');
+        t.deepEqual(actual, expected, 'Returns correct user data if login successful');
+      })
+      .then(() =>
+        checkLogin('samm@gmail.com', 'password'))
+      .then((actual) => {
+        t.equal(actual, undefined, 'Returns undefined if email incorrect');
+      })
+      .then(() =>
+        checkLogin('sam@gmail.com', 'password1'))
+      .then((actual) => {
+        t.equal(actual, undefined, 'Returns undefined if password incorrect');
+      })
+      .catch(console.log);
+  });
 });
