12cbc482e561fd4c1b28ed7976fba35d968da046
Dakshina Scott <dakshina.scott@gmail.com>
2018-01-31 12:27:50 +0000

    #49 adds users_groups to db for memberships

--- database/db_seed.sql
+++ database/db_seed.sql
@@ -21,6 +21,14 @@ BEGIN;
         budget INTEGER
     );
 
+    DROP TABLE IF EXISTS users_groups CASCADE;
+
+    CREATE TABLE users_groups (
+        user_id INTEGER REFERENCES groups(id) ON DELETE CASCADE NOT NULL,
+        group_id INTEGER REFERENCES groups(id) ON DELETE CASCADE NOT NULL,
+        CONSTRAINT users_groups_pkey PRIMARY KEY (user_id, group_id)
+    );
+
 
     INSERT INTO users VALUES
         (DEFAULT, 'sam', 'sam@gmail.com', '$2a$10$CEicRuoB3hvCnlDx9Of/deXIiRInjoRhYuC9VKdox7n0zVXMbzJb2');
@@ -34,6 +42,12 @@ BEGIN;
         false,
         to_date('2200-12-25', 'YYYY-MM-DD'),
         10
+    );
+
+    INSERT INTO users_groups VALUES
+    (
+        (SELECT id FROM users WHERE email = 'sam@gmail.com'),
+        (SELECT id FROM groups WHERE name = 'The best group evaaz')
     );
 
 COMMIT;
